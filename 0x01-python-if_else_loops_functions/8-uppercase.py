eed3d1be4667b4e8584ce87c7a53d6256862c737
GunterPearson <1715@holbertonschool.com>
2020-04-28 23:52:01 +0000

    pushing file 8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,8 @@
 def uppercase(str):
     for s in str:
         if ord(s) >= 97 and ord(s) <= 122:
-            s = chr(ord(s) - 32)
-        print(s, end='')
+            x = chr(ord(s) - 32)
+        else:
+            x = s    
+        print(x, end='')
     print("")
