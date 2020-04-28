90a792ed6aec8431f966439935aac8e37958f35f
GunterPearson <1715@holbertonschool.com>
2020-04-28 18:42:43 +0000

    pushing file 1-last_digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    newnum = -number
-else:
-    newnum = number
-num = newnum % 10
+new = abs(number) % 10
 contain = ["and is greater than 5", "and is 0", "and is less than 6 and not 0"]
-if num > 5:
+if number < 0:
+    new = -new
+if new > 5:
     temp = contain[0]
-if num == 0:
+if new == 0:
     temp = contain[1]
-if num < 6 and num != 0:
+if new < 6 and new != 0:
     temp = contain[2]
-print("Last digit of", number, "is", num, temp)
+print("Last digit of", number, "is", new, temp)
