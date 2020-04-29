8ea23de155a09197975eb666ecdbb29e7b14a510
GunterPearson <1715@holbertonschool.com>
2020-04-29 00:33:33 +0000

    pushing file 13-insert

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,20 +3,5 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-  listint_t *temp;
-  listint_t *temp2 = head;
-
-  if (head == NULL)
-    return (NULL);
-  temp->next = NULL;
-  temp->n = number;
-  while (temp2)
-    {
-      if (temp2->next == NULL)
-	break;
-      temp2 = temp2->next;
-    }
-  temp2->next = temp;
-  head = temp2;
   return (head);
 }
