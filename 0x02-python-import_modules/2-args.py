6a7250fb37a337a1f1e9a6f79cfa1fa25db20292
GunterPearson <1715@holbertonschool.com>
2020-04-30 17:55:49 +0000

    corrected edge cases

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     if arg_len == 1:
         print("0 arguments.")
     elif arg_len > 1:
-        print("{} arguments:".format(arg_len - 1))
+        if arg_len == 2:
+            print("{} argument:".format(arg_len - 1))
+        else:
+            print("{} arguments:".format(arg_len - 1))
         for x in range(1, arg_len):
             print("{}: {}".format(x, sys.argv[x]))
