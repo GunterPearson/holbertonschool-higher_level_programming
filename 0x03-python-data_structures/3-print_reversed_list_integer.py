f9800ecfc3dc1472016ada9aebf308552d43697d
GunterPearson <1715@holbertonschool.com>
2020-05-05 19:03:15 +0000

    pushing file 3-print

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list == []:
-        return 0
+        print('', end='')
     else:
         my_list.reverse()
         for x in range(len(my_list)):
