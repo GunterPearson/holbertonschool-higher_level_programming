37d27d7b85e02ec311d2c97543237518a0cacd8b
GunterPearson <1715@holbertonschool.com>
2020-05-05 19:57:46 +0000

    pushing file 6-print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         print('')
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
-            if y < 2:
+            if y < len(matrix[x]) - 1:
                 print("{:d}".format(matrix[x][y]), end=" ")
-            if y >= 2:
+            if y >= len(matrix[x]) - 1:
                 print("{:d}".format(matrix[x][y]))
