9ce5277ec734dee8cf0f8034b876c0a2a5d3c216
GunterPearson <1715@holbertonschool.com>
2020-05-08 00:36:13 +0000

    pushing file 10-best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == {} or a_dictionary == None:
+    if a_dictionary == {} or a_dictionary is None:
         return None
     lis = list(a_dictionary.keys())[0]
     for key in a_dictionary.keys():
