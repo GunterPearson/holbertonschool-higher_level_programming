6b4479dfd685bfb2485766fe113546c337397f84
GunterPearson <1715@holbertonschool.com>
2020-05-18 22:04:14 +0000

    pushing file 101-safe

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
     import sys
-    i = 0
     try:
-        a, b = args
-        i = fct(a, b)
-        return i
+        return fct(*args)
     except Exception as exp:
         string = "Exception: " + str(exp) + '\n'
         sys.stderr.write(string)
