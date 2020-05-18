d98a32bb509cb215a7bb76290532b6dbe0aa14f3
GunterPearson <1715@holbertonschool.com>
2020-05-18 18:07:24 +0000

    pushing file list division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = [list_length]
+    quot = 0
+    for i in range(list_length):
+        try:
+            quot = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("out of range")
+            quot = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            quot = 0
+        except TypeError:
+            print("wrong type")
+            quot = 0
+        finally:
+            new_list.append(quot)
+    return new_list
