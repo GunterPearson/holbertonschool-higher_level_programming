910eac4ffde9e788006d24e36909d159db60ad60
GunterPearson <1715@holbertonschool.com>
2020-05-18 17:07:35 +0000

    pushing file 0-safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[y]), end="")
             y += 1
-            if y == x:
-                print()
         except:
             print()
             return y
+    print()
     return y
