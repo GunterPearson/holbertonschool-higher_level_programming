7ebf9168848e885fe268ddbc5dc0b34a9cc2e402
GunterPearson <1715@holbertonschool.com>
2020-05-18 17:15:47 +0000

    pushing file 2-safe

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,6 +2,8 @@
 def safe_print_list_integers(my_list=[], x=0):
     y = 0
     i = 0
+    if my_list is None:
+        return i
     while y < x:
         try:
             print("{:d}".format(my_list[y]), end="")
