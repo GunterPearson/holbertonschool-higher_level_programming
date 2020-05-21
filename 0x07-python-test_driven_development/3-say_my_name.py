a66c1e9f666dea34b37146f73f1dec4c8466abb9
GunterPearson <1715@holbertonschool.com>
2020-05-21 22:30:54 +0000

    pushing 3-say and test file

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Say_my_name module
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    print out first and last name in sentence
+    """
+    if type(first_name) is not str or first_name is None:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
