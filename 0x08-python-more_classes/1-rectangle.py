067a4148f0a3e737a7363d4196db9e8a7007a9b8
GunterPearson <1715@holbertonschool.com>
2020-05-22 15:29:21 +0000

    pushing file 1-rec

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Class Rectangle:
+creating new class rectangle
+no methods yet
+"""
+
+
+class Rectangle:
+    """ gets private width """
+    @property
+    def width(self):
+        return self.__width
+    """ sets private width """
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    """ gets private height """
+    @property
+    def height(self):
+        return self.__height
+    """ sets private height """
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+    """ public setter """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
