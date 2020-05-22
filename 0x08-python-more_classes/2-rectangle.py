b88ebf139c8a0b7679e5597378459544045184b6
GunterPearson <1715@holbertonschool.com>
2020-05-22 15:35:36 +0000

    pushing file 2-rec

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Class Rectangle:
+creating new class rectangle
+no methods yet
+"""
+
+
+class Rectangle:
+    """ gets private width """
+    @property
+    def width(self):
+        return self.__width
+    """ sets private width """
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    """ gets private height """
+    @property
+    def height(self):
+        return self.__height
+    """ sets private height """
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+    """ public setter """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    """ public area """
+    def area(self):
+        return self.width * self.height
+    """ public perim """
+    def perimeter(self):
+        return self.width * 2 + self.height * 2
