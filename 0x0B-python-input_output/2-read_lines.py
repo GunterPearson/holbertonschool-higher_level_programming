7c95890d952a3e98cc4a987e89f2d77f29857efb
GunterPearson <1715@holbertonschool.com>
2020-06-04 03:39:03 +0000

    fixed bugs

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,10 +7,12 @@ read x number of lines
 def read_lines(filename="", nb_lines=0):
     """ reads x number of lines """
     i = 0
-    with open(filename, mode="r", encoding="utf-8") as f:
-        if nb_lines <= 0:
+    if nb_lines <= 0:
+        with open(filename, mode="r", encoding="utf-8") as f:
             print(f.read())
-        for line in f:
-            if i < nb_lines:
-                print(line.rstrip())
-            i += 1
+    else:
+        with open(filename, mode="r", encoding="utf-8") as f:
+            for line in f:
+                if i < nb_lines:
+                    print(line.rstrip())
+                i += 1
