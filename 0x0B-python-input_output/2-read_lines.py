238e8d57c9be41c67d8e1ceb575db1fc8f32216b
GunterPearson <1715@holbertonschool.com>
2020-06-04 03:43:38 +0000

    pushing file 2-read

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,11 +7,10 @@ read x number of lines
 def read_lines(filename="", nb_lines=0):
     """ reads x number of lines """
     i = 0
-    if nb_lines <= 0:
-        with open(filename, mode="r", encoding="utf-8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
+        if nb_lines <= 0:
             print(f.read())
-    else:
-        with open(filename, mode="r", encoding="utf-8") as f:
+        else:
             for line in f:
                 if i < nb_lines:
                     print(line.rstrip())
