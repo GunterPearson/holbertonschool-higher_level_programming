5d0890ec7abe9e833c057c4c998c164a8479af03
GunterPearson <1715@holbertonschool.com>
2020-06-04 03:35:15 +0000

    corrected bugs

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ Read file
 def read_file(filename=""):
     """Reading file"""
     with open(filename, mode="r", encoding='UTF8') as files:
-        print(files.read())
+        print(files.read(), end="")
