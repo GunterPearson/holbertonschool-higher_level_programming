3e9c42adbb2fcf65d676127c8556a94832048d16
GunterPearson <1715@holbertonschool.com>
2020-06-05 17:51:27 +0000

    pushing early stages

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+create class base
+"""
+
+
+class Base:
+    """ privat object """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ init function """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
