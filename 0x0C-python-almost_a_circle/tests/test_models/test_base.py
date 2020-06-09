a1fb2ae2e6d848cbe5b90016400b34b93224a178
GunterPearson <1715@holbertonschool.com>
2020-06-09 17:49:19 +0000

    pushing file test_base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,67 +11,68 @@ class TestBase_instantiation(unittest.TestCase):
     """ First base tests """
 
     def test_no_arg(self):
+        """ test no arguments """
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id, b2.id - 1)
 
     def test_three_bases(self):
+        """ test id value """
         b1 = Base()
         b2 = Base()
         b3 = Base()
         self.assertEqual(b1.id, b3.id - 2)
 
     def test_None_id(self):
+        """ test id if none is passed """
         b1 = Base(None)
         b2 = Base(None)
         self.assertEqual(b1.id, b2.id - 1)
 
-    def test_unique_id(self):
+    def test_un_id(self):
+        """ test uniq id """
         self.assertEqual(12, Base(12).id)
 
-    def test_nb_instances_after_unique_id(self):
+    def test_after_unique_id(self):
+        """ test after uniq """
         b1 = Base()
         b2 = Base(12)
         b3 = Base()
         self.assertEqual(b1.id, b3.id - 1)
 
     def test_id_public(self):
+        """ test public id """
         b = Base(12)
         b.id = 15
         self.assertEqual(15, b.id)
 
     def test_nb_instances_private(self):
+        """ test private """
         with self.assertRaises(AttributeError):
             print(Base(12).__nb_instances)
 
     def test_str_id(self):
+        """ test str """
         self.assertEqual("hello", Base("hello").id)
 
     def test_float_id(self):
+        """ test float """
         self.assertEqual(5.5, Base(5.5).id)
 
     def test_dict_id(self):
+        """ test dict """
         self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
 
     def test_bool_id(self):
+        """ test bool """
         self.assertEqual(True, Base(True).id)
 
     def test_list_id(self):
+        """ test list """
         self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
 
-    def test_tuple_id(self):
-        self.assertEqual((1, 2), Base((1, 2)).id)
-
-    def test_set_id(self):
-        self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
-
-    def test_frozenset_id(self):
-        self.assertEqual(frozenset({1, 2, 3}), Base(frozenset({1, 2, 3})).id)
-
-    def test_range_id(self):
-        self.assertEqual(range(5), Base(range(5)).id)
-
     def test_two_args(self):
+        """ test two """
         with self.assertRaises(TypeError):
             Base(1, 2)
 
