eedcac9ad16e1b7fa5d6f0a59b61af011a4eac83
GunterPearson <60362899+GunterPearson@users.noreply.github.com>
2020-06-09 11:25:45 -0500

    updated rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,10 +16,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ return width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ set width """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -28,10 +30,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """ return height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ set hieght """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -40,10 +44,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ return private x value """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ set x value """
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -52,10 +58,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """ return private y """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ sets y value """
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -65,3 +73,60 @@ class Rectangle(Base):
     def area(self):
         """ returns area """
         return self.width * self.height
+
+    def display(self):
+        """ prints self in # """
+        for x in range(self.y):
+            print()
+        st = " " * self.x
+        for x in range(self.height):
+            print(st, end="")
+            for x in range(self.width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """ control what gets printed """
+        return "[{}] ({}) {}/{} - {}/{}".format(
+            type(self).__name__, self.id, self.x, self.y,
+            self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """ update using *args """
+        x = 0
+        if args and len(args) != 0:
+            l = [self.id, self.width, self.height, self.x, self.y]
+            for arg in args:
+                if x > 4:
+                    break
+                if arg is None:
+                    self.__init__(self.width, self.height, self.x, self.y)
+                else:
+                    self.id = arg
+                l[x] = arg
+                x += 1
+            l = tuple(l)
+            self.id, self.width, self.height, self.x, self.y = l
+        elif kwargs and len(kwargs) != 0:
+            l = ["id", "width", "height", "x", "y"]
+            n = [self.id, self.width, self.height, self.x, self.y]
+            for key, value in kwargs.items():
+                for b in range(len(l)):
+                    if b > 4:
+                        break
+                    if key == l[b]:
+                        n[b] = value
+            n = tuple(n)
+            if l[0] == "id":
+                self.__init__(self.width, self.height, self.x, self.y)
+            self.id, self.width, self.height, self.x, self.y = n
+
+    def to_dictionary(self):
+        """ return dict """
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
