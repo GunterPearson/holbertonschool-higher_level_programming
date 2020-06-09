4d6c3c44191f356099d3a636da92587a73e65156
GunterPearson <1715@holbertonschool.com>
2020-06-09 19:26:24 +0000

    added class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -53,3 +53,14 @@ class Base:
             new = cls(1)
         new.update(**dictionary)
         return new
+
+    @classmethod
+    def load_from_file(cls):
+        """ returns a list of instances: """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                o = cls.from_json_string(file.read())
+                return [cls.create(**x) for x in o]
+        except:
+            return []
