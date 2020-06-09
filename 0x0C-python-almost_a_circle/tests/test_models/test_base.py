9a6e109002f4f2712d10e41e7de296b87b4cafc1
GunterPearson <1715@holbertonschool.com>
2020-06-09 18:11:29 +0000

    had to restart on test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-"""Module to test base
+"""Module to test base.Base
 """
 import unittest
-from models.base import Base
+from models import base
 
 
-class TestBase_Methods(unittest.TestCase):
-    """Testing Base class"""
+class Testbase_Methods(unittest.TestCase):
+    """Testing base.Base class"""
 
     def test_id(self):
         """ test different id """
 
-        b1 = Base()
+        b1 = base.Base()
         start = b1.id
-        b2 = Base()
-        b3 = Base()
+        b2 = base.Base()
+        b3 = base.Base()
         self.assertEqual(b2.id, start + 1)
         self.assertEqual(b3.id, start + 2)
 
     def test_id(self):
         """ test id with id set """
-        b1 = Base()
-        b2 = Base(5)
-        b3 = Base()
+        b1 = base.Base()
+        b2 = base.Base(5)
+        b3 = base.Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 5)
         self.assertEqual(b3.id, 2)
 
     def test_unique_id(self):
         """ test uniq """
-        b1 = Base()
-        b2 = Base(12)
-        b3 = Base()
+        b1 = base.Base()
+        b2 = base.Base(12)
+        b3 = base.Base()
         self.assertEqual(b1.id, b3.id - 1)
 
     def test_to_json(self):
