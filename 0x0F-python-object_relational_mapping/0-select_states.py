fb920c879a67541dd63a2ab5d51ccadd1db534d3
GunterPearson <1715@holbertonschool.com>
2020-08-10 15:45:47 +0000

    pushing file 0-select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Select states"""
-import MySQLdb
-from sys import argv
+""" lists all states from hbtn_0e_0_usa """
+import MySQLdb as mysql
+import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="127.0.0.1", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    Cur = conn.cursor()
-    Cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    Rows = Cur.fetchall()
-    for row in Rows:
+    conn = mysql.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-    Cur.close()
+    cur.close()
     conn.close()
