ce0f9bc3480dc32d118d530c7a66ed24dc8a83b1
GunterPearson <1715@holbertonschool.com>
2020-08-10 16:16:20 +0000

    pushing file 1-filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" lists all states from hbtn_0e_0_usa """
+import MySQLdb as mysql
+import sys
+
+if __name__ == "__main__":
+    conn = mysql.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^[n]'")
+    for row in cur.fetchall():
+        print(row[0], " ", row[1])
