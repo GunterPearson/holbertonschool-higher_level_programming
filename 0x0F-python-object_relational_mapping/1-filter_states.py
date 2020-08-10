b79a85acea815097368a74ab40818fc1f9f9fa35
GunterPearson <1715@holbertonschool.com>
2020-08-10 16:21:34 +0000

    pushing file 1-filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     conn = mysql.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name REGEXP '^[n]'")
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^[n]' ORDER BY id ASC")
     for row in cur.fetchall():
-        print(row[0], " ", row[1])
+        print(row)
