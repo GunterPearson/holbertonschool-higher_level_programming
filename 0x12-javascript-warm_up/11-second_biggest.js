e2c1043f785294efed515abfef50e24525ccdfd5
GunterPearson <1715@holbertonschool.com>
2020-09-15 14:20:48 -0500

    pushing file 11-second

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const arg = process.argv;
+arg.splice(0, 2);
+function second () {
+  if (arg.length < 2) {
+    return (0);
+  } else {
+    const ret = arg.sort();
+    return (parseInt(ret.splice(-2, 1)));
+  }
+}
+console.log(second(arg));
