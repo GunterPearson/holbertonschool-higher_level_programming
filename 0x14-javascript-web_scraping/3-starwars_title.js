f14836783492f04c2ef1e3bb7674d6080d96996d
GunterPearson <1715@holbertonschool.com>
2020-09-22 15:54:24 -0500

    pushing file 3-starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 const id = process.argv[2];
-const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + id,
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-    'Accept-Charset': 'utf-8',
-    'User-Agent': 'gunter'
-  }
-};
-request(options, function (err, res, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+request(url, (err, res, body) => {
   const json = JSON.parse(body);
   console.log(json.title);
 });
